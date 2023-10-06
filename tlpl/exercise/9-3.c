2155c3d59c4776031df6d73d0a09b253540053da
Nguyễn Xuân Thứ <thu.nguyen9@tiki.vn>
2023-10-06 16:35:16 +0700

    Full implement 9-3

--- tlpl/exercise/9-3.c
+++ tlpl/exercise/9-3.c
@@ -10,36 +10,69 @@ struct passwd *userFromName(const char *name); // Khai báo nguyên mẫu hàm, 
 
 int initgroups_impl(const char *user, gid_t group)
 {
-    struct passwd *userID;
-    userID = userFromName(user);
-    if (userID == NULL)
+    errno = 0;
+    uid_t euid = geteuid();
+    if (euid != 0)
     {
+        errno = EPERM;
         return -1;
     }
 
-    userID->pw_gid;
+    gid_t *supplementalGroupIDs;
+    long maxGroupSize;
+    maxGroupSize = sysconf(_SC_NGROUPS_MAX);
+    supplementalGroupIDs = malloc(sizeof(gid_t) * maxGroupSize);
+
+    //  struct passwd *userID;
+    // userID = userFromName(user);
+    // if (userID == NULL)
+    // {
+    //     return -1;
+    // }
 
     printf("Start looking in group\n");
 
     struct group *grp;
-    char **grMem;
+    char mem;
+    size_t size = 0;
+    int currentIndex = 0;
+    setgrent();
     while ((grp = getgrent()) != NULL)
     {
-        grMem = grp->gr_mem;
-        if (*grMem != NULL)
+        for (size_t j = 0;; j++)
         {
-            printf("Value group (%s) mem: %s \n", grp->gr_name, *grMem);
-        }
+            if (grp->gr_mem[j] == NULL)
+            {
+                break;
+            }
 
-        // while (*grMem != NULL)
-        // {
-        //     if (strcmp(**grMem, user) == 0)
-        //     {
-        //         printf("Catched you in group %s", grp->gr_name);
-        //     }
-        // }
+            if ((strcmp(grp->gr_mem[j], user)) == 0)
+            {
+                printf("Found in group id %d name %s \n", grp->gr_gid, grp->gr_name);
+                supplementalGroupIDs[currentIndex] = grp->gr_gid;
+                size += 1;
+                currentIndex += 1;
+            }
+        }
     }
     endgrent();
+
+    printf("Supplemental group ids\n");
+    for (int i = 0; i < currentIndex; i++)
+    {
+        printf("%d ", supplementalGroupIDs[i]);
+    }
+    printf("\n");
+
+    printf("Size to set group: %lu\n", size);
+    if (setgroups(size, supplementalGroupIDs) == -1)
+    {
+        if (errno != 0)
+        {
+            perror("Problem");
+        }
+        return -1;
+    }
 
     return 0;
 }
@@ -65,7 +98,13 @@ int main(int argc, char const *argv[])
     int n;
     char *username;
     username = getlogin();
+    printf("Check username: %s\n", username);
     n = initgroups_impl(username, 0);
-    printf("%d", n);
+    printf("%d\n", n);
+    
+    if (n == -1) {
+        perror("initgroups_impl failed");
+    }
+    
     return 0;
 }
