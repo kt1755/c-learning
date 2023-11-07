03d914be2b0e56efc9d9ec499d1777972841004e
Nguyễn Xuân Thứ <thu.nguyen9@tiki.vn>
2023-11-07 22:29:21 +0700

    Add more

---
+++ tlpi/exercise/copy.c
@@ -0,0 +1,67 @@
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <errno.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <string.h>
+// #include "../lib/error_functions.h"
+
+// #include "tlpi_hdr.h"
+
+#ifndef BUF_SIZE  /* Allow "cc -D" to override definition */
+#define BUF_SIZE 1024
+#endif
+
+int main(int argc, char *argv[])
+{
+    int inputFd, outputFd, openFlags;
+    mode_t filePerms;
+    ssize_t numRead;
+
+printf("Buf size: %d\n", BUF_SIZE);
+
+    char buf[BUF_SIZE];
+    if (argc < 3 || strcmp(argv[1], "--help") == 0)
+    {
+        // printf("%s old-file new-file\n", argv[0]);
+        printf("argc: %d \n", argc);
+        return -1;
+    }
+
+    /* Open input and output files */
+    inputFd = open(argv[1], O_RDONLY);
+    if (inputFd == -1)
+    {
+        printf("opening file %s", argv[1]);
+        return -1;
+    }
+    openFlags = O_CREAT | O_WRONLY | O_TRUNC | O_SYNC;
+    filePerms = S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP |
+                S_IROTH | S_IWOTH ; /* rw-rw-rw- */
+
+    outputFd = open(argv[2], openFlags, filePerms);
+    if (outputFd == -1)
+    {
+        // printf("opening file %s", argv[2]);
+        perror(argv[2]);
+                return -1;
+    }
+    /* Transfer data until we encounter end of input or an error */
+    while ((numRead = read(inputFd, buf, BUF_SIZE)) > 0)
+        if (write(outputFd, buf, numRead) != numRead)
+        {
+            printf("couldn't write whole buffer");
+            return -1;
+        }
+
+    if (numRead == -1)
+        perror("read");
+
+    if (close(inputFd) == -1)
+        perror("close input");
+
+    if (close(outputFd) == -1)
+        perror("close output");
+
+    return 0;
+}
---
+++ tlpi/exercise/tail.c
@@ -0,0 +1,12 @@
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <errno.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <string.h>
+
+int main(int argc, char const *argv[])
+{
+    /* code */
+    return 0;
+}
