c09ea97e1bb631e6ea7212c79690ed46d70a9e95
Nguyễn Xuân Thứ <thu.nguyen9@tiki.vn>
2023-12-07 23:14:25 +0700

    Add create random file 14-1

--- tlpi/exercise/14-1.c
+++ tlpi/exercise/14-1.c
@@ -6,8 +6,10 @@
 #include <string.h>
 #include <stdlib.h>
 #include <fcntl.h>
+#include <time.h>
 
-int main(int argc, char const *argv[])
+
+int main(int argc, char *argv[])
 {
 
     char *numOfFileStr;
@@ -26,21 +28,58 @@ int main(int argc, char const *argv[])
         }
     }
 
+    printf("Generate %s file in %s\n", numOfFileStr, filePath);
+
+    struct stat *directoryStat;
+    stat(filePath, directoryStat);
+
     int numOfFile = atoi(numOfFileStr);
     int fileNameNum = 0;
-    char *fullPath;
-    char *fileName;
-
-    srand(time(NULL));
-    for (int i = 0; i < numOfFile; i++)
-    {
-        fileNameNum = rand() % numOfFile;
-        sprintf(fileName, "x%6d", fileNameNum);
-        sprintf(fullPath,"%s/%s", filePath, fileName);
-
-        int fd = open(fullPath, O_CREAT | O_RDWR);
+    char *fullPath = malloc(255 *sizeof(char));
+    char *fileName = malloc(255 *sizeof(char));
+    char *fileArr[numOfFile];
+    int generatedNum[numOfFile];
+    for (int i = 0; i < numOfFile; i++) {
+        generatedNum[i] = i+1;
     }
 
+    srand(time(NULL));
+    // Shuffle the array to create random order
+    int tmp = 0;
+    for (int i = 0; i < numOfFile; i++)
+    {
+        int j = rand() % numOfFile + 1;
+
+        tmp =  generatedNum[j];
+        generatedNum[j] = generatedNum[i];
+        generatedNum[i] = tmp;
+    }
+
+
+
+    for (int i = 0; i < numOfFile; i++)
+    {
+        
+        fileNameNum = generatedNum[i];
+        sprintf(fileName, "x%06d", fileNameNum); //segment fault
+        sprintf(fullPath,"%s/%s", filePath, fileName);
+
+        // int fd = open(fullPath, O_CREAT | O_RDWR);
+        // if (fd == -1) {
+        //     perror("cannot open file");
+        //     exit(EXIT_FAILURE);
+        // }
+
+        // ftruncate(fd, 1);
+        // close(fd);
+
+        // fileArr[i] = fullPath;
+
+        printf("Random at %d: %s\n", i, fullPath);
+    }
+
+    
     /* code */
     return 0;
 }
+
