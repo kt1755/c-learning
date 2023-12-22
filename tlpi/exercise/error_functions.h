0757133485a36d57cf6af75cd71a0f070624191c
Nguyễn Xuân Thứ <thu.nguyen9@tiki.vn>
2023-12-22 22:08:48 +0700

    Thêm các file của sách

--- tlpi/exercise/error_functions.h
+++ tlpi/exercise/error_functions.h
@@ -1,113 +1,45 @@
-#include <stdarg.h>
-#include "error-diagnose.h"
-#include "tlpi_hdr.h"
+/*************************************************************************\
+*                  Copyright (C) Michael Kerrisk, 2023.                   *
+*                                                                         *
+* This program is free software. You may use, modify, and redistribute it *
+* under the terms of the GNU Lesser General Public License as published   *
+* by the Free Software Foundation, either version 3 or (at your option)   *
+* any later version. This program is distributed without any warranty.    *
+* See the files COPYING.lgpl-v3 and COPYING.gpl-v3 for details.           *
+\*************************************************************************/
 
-/* Defines ename and MAX_ENAME */
+/* error_functions.h
+
+   Header file for error_functions.c.
+*/
+#ifndef ERROR_FUNCTIONS_H
+#define ERROR_FUNCTIONS_H
+
+/* Error diagnostic routines */
+
+void errMsg(const char *format, ...);
+
 #ifdef __GNUC__
-__attribute__((__noreturn__))
+
+    /* This macro stops 'gcc -Wall' complaining that "control reaches
+       end of non-void function" if we use the following functions to
+       terminate main() or some other non-void function. */
+
+#define NORETURN __attribute__ ((__noreturn__))
+#else
+#define NORETURN
 #endif
-static void terminate(Boolean useExit3)
-{
-   char *s;
-   /* Dump core if EF_DUMPCORE environment variable is defined and
-   is a nonempty string; otherwise call exit(3) or _exit(2),
-   depending on the value of 'useExit3'. */
-   s = getenv("EF_DUMPCORE");
-   if (s != NULL && *s != '\0')
-      abort();
-   else if (useExit3)
-      exit(EXIT_FAILURE);
-   else
-      _exit(EXIT_FAILURE);
-}
 
-static void outputError(Boolean useErr, int err, Boolean flushStdout,
-            const char *format, va_list ap)
-{
-#define BUF_SIZE 500
-   char buf[BUF_SIZE], userMsg[BUF_SIZE], errText[BUF_SIZE];
-   vsnprintf(userMsg, BUF_SIZE, format, ap);
-   if (useErr)
-      snprintf(errText, BUF_SIZE, " [%s %s]",
-               (err > 0 && err <= MAX_ENAME) ? ename[err] : "?UNKNOWN?", strerror(err));
-   else
-      snprintf(errText, BUF_SIZE, ":");
-   snprintf(buf, BUF_SIZE, "ERROR%s %s\n", errText, userMsg);
-   if (flushStdout)
-      fflush(stdout);
-   fputs(buf, stderr);
-   fflush(stderr);
-   /* Flush any pending stdout */
-   /* In case stderr is not line-buffered */
-}
-void errMsg(const char *format, ...)
-{
-   va_list argList;
-   int savedErrno;
-   savedErrno = errno;
-   /* In case we change it here */
-   va_start(argList, format);
-   outputError(TRUE, errno, TRUE, format, argList);
-   va_end(argList);
-   errno = savedErrno;
-}
-void errExit(const char *format, ...)
-{
-   va_list argList;
-   va_start(argList, format);
-   outputError(TRUE, errno, TRUE, format, argList);
-   va_end(argList);
-   terminate(TRUE);
-}
+void errExit(const char *format, ...) NORETURN ;
 
-void err_exit(const char *format, ...)
-{
-   va_list argList;
-   va_start(argList, format);
-   outputError(TRUE, errno, FALSE, format, argList);
-   va_end(argList);
-   terminate(FALSE);
-}
-void errExitEN(int errnum, const char *format, ...)
-{
-   va_list argList;
-   va_start(argList, format);
-   outputError(TRUE, errnum, TRUE, format, argList);
-   va_end(argList);
-   terminate(TRUE);
-}
-void fatal(const char *format, ...)
-{
-   va_list argList;
-   va_start(argList, format);
-   outputError(FALSE, 0, TRUE, format, argList);
-   va_end(argList);
-   terminate(TRUE);
-}
-void usageErr(const char *format, ...)
-{
-   va_list argList;
-   fflush(stdout);
-   /* Flush any pending stdout */
-   fprintf(stderr, "Usage: ");
-   va_start(argList, format);
-   vfprintf(stderr, format, argList);
-   va_end(argList);
-   fflush(stderr);
-   exit(EXIT_FAILURE);
-}
+void err_exit(const char *format, ...) NORETURN ;
 
-/* In case stderr is not line-buffered */
-void cmdLineErr(const char *format, ...)
-{
-   va_list argList;
-   fflush(stdout);
-   /* Flush any pending stdout */
-   fprintf(stderr, "Command-line usage error: ");
-   va_start(argList, format);
-   vfprintf(stderr, format, argList);
-   va_end(argList);
-   fflush(stderr);
-   exit(EXIT_FAILURE);
-   /* In case stderr is not line-buffered */
-}
+void errExitEN(int errnum, const char *format, ...) NORETURN ;
+
+void fatal(const char *format, ...) NORETURN ;
+
+void usageErr(const char *format, ...) NORETURN ;
+
+void cmdLineErr(const char *format, ...) NORETURN ;
+
+#endif
