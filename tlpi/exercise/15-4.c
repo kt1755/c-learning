56c5e14d78c7e621f01b4eb9afbb7410982e90b8
Nguyễn Xuân Thứ <thu.nguyen9@tiki.vn>
2023-12-24 22:31:01 +0700

    Add code 15-4

---
+++ tlpi/exercise/15-4.c
@@ -0,0 +1,29 @@
+# include <stdio.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+
+int euidaccess(const char *pathname, int mode) {
+   uid_t eeuid = geteuid();
+   uid_t eguid = getegid();
+
+   struct stat *statbuf;
+   if (stat(pathname, statbuf) == -1) {
+      // TODO handle error more careful
+      return -1; 
+   }
+
+   if (statbuf.st_uid != eeuid && statbuf->st_gid) {
+      return -1; // No permission
+   }
+
+   if (statbuf.st_mode & S_IRUSR) {
+      
+   }
+}
+
+int main(int argc, char const *argv[])
+{
+   /* code */
+   return 0;
+}
