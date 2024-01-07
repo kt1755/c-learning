84db673650dde13259798eb559720bdc6648e02d
Nguyễn Xuân Thứ <thu.nguyen9@tiki.vn>
2024-01-07 23:45:23 +0700

    Add 17-1 header

--- tlpi/exercise/section_17/17-1.c
+++ tlpi/exercise/section_17/17-1.c
@@ -7,11 +7,14 @@
 #include <sys/xattr.h>
 #include <stdlib.h>
 #include <string.h>
+#include <sys/acl.h> // $ sudo apt-get install libacl1-dev
+#include "ugid_functions.h"
 
 int main(int argc, char *argv[])
 {
     char *type;
     char *idStr;
+    
 
     int opt;
     while ((opt = getopt(argc, argv, ":t:i:")) != -1)
@@ -32,6 +35,28 @@ int main(int argc, char *argv[])
     }
 
     printf("Type affected: %s\n", type);
+    printf("Identity check: %s\n", idStr);
+
+    char *fileName = argv[optind];
+
+    gid_t groupID;
+    uid_t userID;
+
+    switch (type)
+    {
+    case 'u':
+        userID = userIdFromName(idStr);
+        break;
+
+    case 'g':
+        groupID = groupIdFromName(idStr);
+        break;
+
+    default:
+        break;
+    }
+
+    getchar
 
     return 0;
 }
