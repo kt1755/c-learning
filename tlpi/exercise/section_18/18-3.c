2a6c4bdfefc4a523673a0fe4dfaa29fbba29c041
Nguyễn Xuân Thứ <thu.nguyen9@tiki.vn>
2024-01-25 23:27:04 +0700

    Add 18-3

--- tlpi/exercise/section_18/18-3.c
+++ tlpi/exercise/section_18/18-3.c
@@ -4,6 +4,7 @@
 #include <stdbool.h>
 #include <sys/stat.h>
 #include <errno.h>
+#include <string.h>
 
 char *realpathImpl(const char *pathname, char *resolved_path)
 {
@@ -16,34 +17,62 @@ char *realpathImpl(const char *pathname, char *resolved_path)
         return NULL;
     }
 
-    if  (!S_ISLNK (sb.st_mode)) {
+    if (!S_ISLNK(sb.st_mode))
+    {
         printf("Not symlink\n");
         return NULL;
     }
 
     if (resolved_path == NULL)
     {
-        resolved_path == malloc(sizeof(char) * _PC_PATH_MAX);
+        resolved_path = malloc(sizeof(char) * 255);
     }
 
     bool isSymlink = true;
+    printf("%s is symlink %d \n", pathname, isSymlink);
 
     while (isSymlink)
     {
-        size_t size = readlink(pathname, resolved_path, _PC_PATH_MAX);
+        size_t size = readlink(pathname, resolved_path, 255);
         if (size == -1)
         {
             return NULL;
         }
 
-        pathname = resolved_path;
+        printf("pathname %s - resolve_path %s\n", pathname, resolved_path);
+
+        if (lstat(resolved_path, &sb) == -1)
+        {
+            printf("%s is not symlink anymore\n", resolved_path);
+            return NULL;
+        }
+
+        if (!S_ISLNK(sb.st_mode))
+        {
+            isSymlink = false;
+        }
+        else
+        {
+            char *newPathName = calloc(255, sizeof(char));
+            memcpy(newPathName, resolved_path, 255);
+            printf("newPathName = %s\n", newPathName);
+            pathname = newPathName;
+            memset(resolved_path, 0, 255);
+        }
     }
 
-    return NULL;
+    printf("%s is symlink %d \n", pathname, isSymlink);
+
+    return resolved_path;
 }
 
 int main(int argc, char const *argv[])
 {
-    /* code */
-    return 0;
+    char *file = malloc(sizeof(char) * 255);
+
+        realpathImpl(argv[1], file);
+    printf("symlink: %s\n", argv[1]);
+    printf("real path: %s\n", file);
+
+    return EXIT_SUCCESS;
 }
