3f7df6631ff5fa2d44fd5ede3038c139d3081a99
Nguyễn Xuân Thứ <thu.nguyen9@tiki.vn>
2024-02-22 23:17:57 +0700

    Add lib

---
+++ tlpi/exercise/section_18/18-5.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <unistd.h>
+#include "/Users/lap-01124/c-learning/tlpi/lib/tlpi_hdr.h"
+
+char *getcwd_impl(char *cwdbuf, size_t size) {
+    fchdir(".");
+    
+}
+
+int main(int argc, char const *argv[])
+{
+    /* code */
+    return 0;
+}
