ae34b744dcff41d4ae82beab020d04a4f78bb67b
Nguyễn Xuân Thứ <thu.nguyen9@tiki.vn>
2024-03-21 21:17:57 +0700

    Add clock_nanosleep

--- tlpi/exercise/section_23/t_nanosleep.c
+++ tlpi/exercise/section_23/t_nanosleep.c
@@ -15,7 +15,7 @@
 
    See also t_clock_nanosleep.c.
     macos cmd: clang t_nanosleep.c -o t_nanosleep.out ../../lib/signal_functions.c ../../lib/error_functions.c ../../lib/get_num.c -I/Users/lap-01124/c-learning/tlpi/lib
-    ubuntu cmd: gcc t_nanosleep.c -o t_nanosleep.out ../../lib/signal_functions.c ../../lib/error_functions.c ../../lib/get_num.c -I/home/thu/coding/tlpi/lib 
+    ubuntu cmd: gcc t_nanosleep.c -o t_nanosleep.out ../../lib/signal_functions.c ../../lib/error_functions.c ../../lib/get_num.c -I/home/thu/coding/tlpi/lib
 
 */
 #define _POSIX_C_SOURCE 199309
@@ -42,7 +42,7 @@ sigintHandler(int sig)
 int main(int argc, char *argv[])
 {
     struct timeval start, finish;
-    struct timespec request, remain, startspec, finishspec;
+    struct timespec request, remain, startspec, finishspec, requestabs;
     struct sigaction sa;
     int s;
 
@@ -51,6 +51,8 @@ int main(int argc, char *argv[])
 
     request.tv_sec = getLong(argv[1], 0, "secs");
     request.tv_nsec = getLong(argv[2], 0, "nanosecs");
+
+    printf("Request for: %9ld secs\n", request.tv_sec);
 
     /* Allow SIGINT handler to interrupt nanosleep() */
 
@@ -66,35 +68,42 @@ int main(int argc, char *argv[])
     if (clock_gettime(CLOCK_REALTIME, &startspec) == -1)
         errExit("clock_gettime start");
 
+    requestabs.tv_sec = request.tv_sec + startspec.tv_sec;
+    requestabs.tv_nsec = request.tv_nsec + startspec.tv_nsec;
+
     for (;;)
     {
-        /* Using clock_nanosleep instead
+        // Using clock_nanosleep instead
 
-        s = nanosleep(&request, &remain);
-        if (s == -1 && errno != EINTR)
-            errExit("nanosleep");
-        */
+        // s = nanosleep(&request, &remain);
+        // if (s == -1 && errno != EINTR)
+        //     errExit("nanosleep");
 
-        // Continue implement
-        s = clock_nanosleep(CLOCK_REALTIME, TIMER_ABSTIME, &request, &remain);
-        if (s == -1 && errno != EINTR)
-            errExit("nanosleep");
+        // printf("Current s: %d", s);
+        
 
         // if (gettimeofday(&finish, NULL) == -1)
         //     errExit("gettimeofday");
         // printf("Slept for: %9.6f secs\n", finish.tv_sec - start.tv_sec +
         //                                       (finish.tv_usec - start.tv_usec) / 1000000.0);
 
+        // Continue implement
+        // Need to pass absolute time as parameter for both request and remain
+        s = clock_nanosleep(CLOCK_REALTIME, TIMER_ABSTIME, &requestabs, &remain);
+        if (s == -1 && errno != EINTR)
+            errExit("nanosleep");
+        
         if (clock_gettime(CLOCK_REALTIME, &finishspec) == -1)
             errExit("clock_gettime start");
         printf("Slept for: %9.6f secs\n", finishspec.tv_sec - startspec.tv_sec +
-                                              (finishspec.tv_sec - startspec.tv_sec) / 1000000.0);    
+                                              (finishspec.tv_sec - startspec.tv_sec) / 1000000.0);
 
         if (s == 0)
             break; /* nanosleep() completed */
 
         printf("Remaining: %2ld.%09ld\n", (long)remain.tv_sec, remain.tv_nsec);
-        request = remain; /* Next sleep is with remaining time */
+        // request = remain; /* Next sleep is with remaining time */
+        requestabs = remain;
     }
 
     printf("Sleep complete\n");
