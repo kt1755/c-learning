1f49fee382b8485b3b98609a8b22d33e337a9825
Nguyễn Xuân Thứ <thu.nguyen9@tiki.vn>
2024-07-31 22:46:35 +0700

    31-1

--- tlpi/exercise/section_31/31-1.c
+++ tlpi/exercise/section_31/31-1.c
@@ -5,14 +5,26 @@
 static pthread_once_t once = PTHREAD_ONCE_INIT;
 
 struct  init_once_t {
-    bool inited;
-    pthread_mutex_t mu;
+    bool *inited;
+    pthread_mutex_t *mu;
 };
 
-static struct init_once_t init_once;
+static struct init_once_t init_once = {false, PTHREAD_MUTEX_INITIALIZER};
 
 int one_time_init(struct init_once_t control, void (*init)(void)) {
+    if ( pthread_mutex_lock(control.mu) > 0) {
+        perror("one_time_init pthread_mutex_lock");
+        return 1;
+    }
 
+    if (!*control.inited) {
+        init();
+        control.inited = true;
+    }
+    if (pthread_mutext_unlock(control.mu) > 0) {
+        perror("one_time_init pthread_mutext_unlock");
+        return 1;
+    };
     return 0;
 }
 
