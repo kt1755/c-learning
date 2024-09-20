ee04eca60874bc460e5272f73ab5615c35dcf289
Nguyễn Xuân Thứ <thu.nguyen9@tiki.vn>
2024-09-20 17:18:57 +0700

    Add 35-1 full

--- tlpi/exercise/section_35/35-1.c
+++ tlpi/exercise/section_35/35-1.c
@@ -3,11 +3,17 @@
 #include <unistd.h>
 #include <sys/resource.h>
 #include <errno.h>
+#include <string.h>
 
-int main(int argc, char const *argv[])
+extern char **environ;
+
+int main(int argc, char *argv[])
 {
     /* code */
     int niceValue;
+    int opt;
+    char *prog = NULL;
+
     errno = 0;
     if (argc == 1)
     {
@@ -24,6 +30,55 @@ int main(int argc, char const *argv[])
 
         printf("Nice value of current process is %d\n", niceValue);
         return EXIT_SUCCESS;
+    }
+
+    while ((opt = getopt(argc, argv, "n:")) != -1)
+    {
+        switch (opt)
+        {
+        case 'n':
+            /* code */
+            niceValue = atoi(optarg);
+            break;
+
+        default:
+            break;
+        }
+    }
+
+    printf("argc = %d\n", argc);
+    printf("optind = %d\n", optind);
+
+    if (nice(niceValue) == -1)
+    {
+        perror("Cannot set nice value myself\n");
+        return EXIT_FAILURE;
+    }
+
+    niceValue = getpriority(PRIO_PROCESS, 0);
+    printf("Priority after set nice value: %d\n", niceValue);
+
+    if (optind < argc)
+    {
+        prog = argv[optind];
+        printf("Program to execute: %s\n", prog);
+
+        char *progArgs = malloc(sizeof(char)* 255);
+        int index = 0;
+        for (int i = optind + 1; i < argc; i++)
+        {
+            printf("Argv %d = %s\n", i, argv[i]);
+            progArgs[index] = *argv[i];
+            index += 1;
+        }
+
+        printf("Param %s\n", progArgs);
+
+        if (execl(prog, progArgs) == -1)
+        {
+            perror("execl failed");
+            return EXIT_FAILURE;
+        }
     }
 
     return EXIT_SUCCESS;
