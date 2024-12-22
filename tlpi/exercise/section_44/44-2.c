28171a83311a9da20c602eed7a3390ebf5292335
thu <thunguyenxuan.3003@gmail.com>
2024-12-22 23:49:11 +0700

    Add 44-2

---
+++ tlpi/exercise/section_44/44-2.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+FILE *popenimpl(const char *command, const char *mode) {
+    return NULL;
+}
+
+int pcloseimpl(FILE *stream){
+    return 0;
+}
+
+int main(int argc, char const *argv[])
+{
+    /* code */
+    return 0;
+}
